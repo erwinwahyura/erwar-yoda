bbe25678dddb8326869450f15f27f7b297d3dc74
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-07-19 12:54:17 +0700

    add unirest

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var unirest = require('unirest');
+
 module.exports = function erwarYoda (sentence) {
   unirest.get(`https://yoda.p.mashape.com/yoda?sentence=${sentence}`)
   .header("X-Mashape-Key", "QGOXQtQCgkmshQZ216bcDj8RaRX4p1yguhIjsn88GLh3e3Iybw")
