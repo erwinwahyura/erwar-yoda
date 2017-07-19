aec6ce66e31b63d4aac385b6486584581cf97140
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-07-19 11:57:05 +0700

    erwarYoda sepak

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function erwarYoda (sentence) {
+  unirest.get(`https://yoda.p.mashape.com/yoda?sentence=${sentence}`)
+  .header("X-Mashape-Key", "QGOXQtQCgkmshQZ216bcDj8RaRX4p1yguhIjsn88GLh3e3Iybw")
+  .header("Accept", "text/plain")
+  .end(function (result) {
+    console.log(result.status, result.headers, result.body);
+  });
+}
